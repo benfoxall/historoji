38feaaf6d5fb915dc836568beaef16778bd591ff
Ben Foxall <ben@pusher.com>
2016-02-06 23:22:46 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 require('dotenv').config({silent: true})
 
 const pg = require('pg')
@@ -8,39 +10,45 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.post('/', bp, (req, res) => {
+app.post('/', bp, (req, res, next) => {
 
   console.log(req.body)
 
-  emojis(req.body.text)
-  .forEach(function(e){
-    console.log(">>>", e)
-    // should probably be the other way around
-    if(process.env.DATABASE_URL) {
-      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  const token   = req.body.token
+  const domain  = req.body.team_domain
+  const channel = req.body.channel_name
+  const user    = req.body.user_name
+  const emoji   = emojis(req.body.string)
 
-        if(err) return console.error('error fetching client from pool', err);
+  if(!(domain || channel || user))
+    return res.sendStatus(400)
 
-        client.query({
-          text: 'INSERT INTO "public"."emoji"("emoji", "team", "channel", "user") VALUES($1, $2, $3, $4) RETURNING "id", "emoji", "team", "channel", "user", "time"',
-          values: [
-            e,
-            req.body.team_domain,
-            req.body.channel_name,
-            req.body.user_name
-          ]
-        }, function(err, result) {
-          if(err) {
-            return console.error('error running query', err);
-          }
-          console.log("saved>", result.rows[0]);
-          done()
-        });
-      })
-    }
+  if(!emoji.length)
+    return res.sendStatus(202)
+
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+
+    if(err) return console.error('error fetching client from pool', err);
+
+    let count = emoji.length
+
+    emoji.forEach((em) => {
+      client.query({
+        text: 'INSERT INTO "public"."emoji"("emoji", "team", "channel", "user") VALUES($1, $2, $3, $4) RETURNING "id", "emoji", "team", "channel", "user", "time"',
+        values: [
+          e,
+          req.body.team_domain,
+          req.body.channel_name,
+          req.body.user_name
+        ]
+      }, function(err, result) {
+        if(!--count) {done(); res.sendStatus(200)}
+        if(err) return next(err)
+
+        console.log("saved>", result.rows[0]);
+      });
+    })
   })
-
-  res.send('cool')
 })
 
 app.get('/data', (req, res, next) => {
