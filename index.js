73223d92e45d314b069e16898fa864383ce8afdd
Ben Foxall <ben@pusher.com>
2016-02-06 21:50:48 +0000

    avoid block scoping issues

--- index.js
+++ index.js
@@ -12,28 +12,18 @@ app.post('/', bp, (req, res) => {
 
   console.log(req.body)
 
-
-  // todo: if req.body.token match
-
-
-  //
-
-  const RE = /:(\w+):/g
-
-  var m
-  while(m = RE.exec(req.body.text)){
-    console.log("MATCH:", m)
-  }
-
-  for(e of emojis(req.body.text)) {
-    console.log(">>>>", e)
-
+  emojis(req.body.text)
+  .forEach(function(e){
+    console.log(">>>", e)
     // should probably be the other way around
     if(process.env.DATABASE_URL) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+
         if(err) {
           return console.error('error fetching client from pool', err);
         }
+        client.on('drain', client.end.bind(client))
+
         client.query({
           text: 'INSERT INTO "public"."emoji"("emoji", "team", "channel", "user") VALUES($1, $2, $3, $4) RETURNING "id", "emoji", "team", "channel", "user", "time"',
           values: [
@@ -43,22 +33,14 @@ app.post('/', bp, (req, res) => {
             req.body.user_name
           ]
         }, function(err, result) {
-          done();
           if(err) {
             return console.error('error running query', err);
           }
           console.log("id:", result.rows[0]);
         });
-      });
+      })
     }
-  }
-
-
-
-
-  //
-  //
-
+  })
 
   res.send('cool')
 })
@@ -70,12 +52,10 @@ app.get('*', (req, res) => {
 
 app.listen(process.env.PORT || 3000)
 
-function* emojis(text) {
+function emojis(text) {
   const RE = /:(\w+):/g
-
+  const _s = []
   var m
-  while(m = RE.exec(text)) {
-    yield m[1]
-  }
-
+  while(m = RE.exec(text)) _s.push(m[1])
+  return _s
 }
