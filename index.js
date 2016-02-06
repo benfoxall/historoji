3883893eeef8629fc627a41a9c69fbec5b690698
Ben Foxall <ben@pusher.com>
2016-02-06 21:20:10 +0000

    like, totally, web

--- index.js
+++ index.js
@@ -12,18 +12,50 @@ app.post('/', bp, (req, res) => {
 
   console.log(req.body)
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
+
+  // todo: if req.body.token match
+
+
+  //
+
+  const RE = /:(\w+):/g
+
+  var m
+  while(m = RE.exec(req.body.text)){
+    console.log("MATCH:", m)
+  }
+
+  for(e of emojis(req.body.text)) {
+    console.log(">>>>", e)
+
+    // should probably be the other way around
+    if(process.env.DATABASE_URL) {
+      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query(`
+          INSERT INTO "public"."emoji"("emoji", "team", "channel", "user")
+          VALUES('{$1}', '{$2}', '{$3}', '{$4}')
+          RETURNING "id", "emoji", "team", "channel", "user", "time"
+          `, [e, req.body.team_domain,
+              req.body.channel_name, req.body.user_name], function(err, result) {
+          done();
+          if(err) {
+            return console.error('error running query', err);
+          }
+          console.log(result.rows[0].number);
+        });
+      });
     }
-    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-      done();
-      if(err) {
-        return console.error('error running query', err);
-      }
-      console.log(result.rows[0].number);
-    });
-  });
+  }
+
+
+
+
+  //
+  //
+
 
   res.send('cool')
 })
@@ -34,3 +66,13 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000)
+
+function* emojis(text) {
+  const RE = /:(\w+):/g
+
+  var m
+  while(m = RE.exec(text)) {
+    yield m[1]
+  }
+
+}
