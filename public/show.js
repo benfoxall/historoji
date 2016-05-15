b175de2353e8547ef4ca978f7597ee279264d058
Ben Foxall <ben@pusher.com>
2016-05-15 12:39:34 +0100

    initial show code

---
+++ public/show.js
@@ -0,0 +1,101 @@
+// populated by fetching
+var emojiMap = {}
+var emojiMapDo = {}
+var data = []
+var w = window.innerWidth
+var h = window.innerHeight
+
+var nope = {}
+
+var circles = []
+
+function show() {
+
+  var min = +Infinity,
+      max = -Infinity
+
+  data.forEach((d) => {
+    min = Math.min(min,d[1])
+    max = Math.max(max,d[1])
+  })
+
+  data.reverse()
+
+  data.forEach((d) => {
+    var shortcode = d[2]
+    if(shortcode == 'simple_smile') shortcode = 'smile'
+
+    var src
+    if(emojiMap[shortcode]){
+      src = `emoji/${emojiMap[shortcode]}`
+    }
+    if(emojiMapDo[shortcode]){
+      src = `emoji-do/${emojiMapDo[shortcode]}`
+    }
+
+    if(src) {
+
+      var img = document.createElement('img')
+      img.src = src
+      img.style.transform = 'scale(0.01)'
+      img.style.transitionDelay = (((d[1] - min) / (max-min)) * 2) + 's'
+
+      document.body.appendChild(img)
+
+      circles.push({
+        time: d[1],
+        x:
+        (((d[1] - min) / (max-min)) * (w-50) * 1) + Math.random() * 50,
+        r: 4,
+        element: img
+      })
+
+      img.onerror = function(){
+        console.log(emojiMap[shortcode])
+      }
+    } else {
+      // not found
+      nope[shortcode] = (nope[shortcode] || 0) + 1
+    }
+  })
+
+  console.log(nope)
+
+
+  //
+  // circles.forEach(c => {c.element.style.left = c.x + 'px'; c.element.style.top = c.y + 'px'; c.element.style.position = 'absolute'})
+  ys = circlepack(circles)
+
+  setTimeout(function(){
+      circles.forEach((c,i) => {
+        c.element.style.left = c.x + 'px';
+        c.element.style.top = (ys[i]+h/2) + 'px';
+        c.element.style.position = 'absolute'
+        c.element.style.transform = 'scale('+Math.random()+')'
+      })
+  },100)
+
+
+
+}
+
+
+
+// populate data
+Promise.all([
+
+  fetch('/emojimap.json')
+    .then(r => r.json())
+    .then(_ => emojiMap = _),
+
+  fetch('/emojimap-do.json')
+    .then(r => r.json())
+    .then(_ => emojiMapDo = _),
+
+  fetch('/data')
+  // fetch('/test-full.json')
+    .then(r => r.json())
+    .then(_ => data = _)
+
+])
+.then(show)
